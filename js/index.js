53e52c858fe54db6e632e7a9ed169f3ba303d7ff
zzc <1257055365@qq.com>
2023-12-21 21:38:00 +0800

    feat:四u给

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+function _toConsumableArray (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i] } return arr2 } else { return Array.from(arr) } }
 
 (function () {
 
     var AUTHOR = {
         AUTHOR: 'author',
         ME: 'me'
-    };
+    }
 
-    var TYPING_MSG_CONTENT = '\n        <div class="dot"></div>\n        <div class="dot"></div>\n        <div class="dot"></div>\n    ';
+    var TYPING_MSG_CONTENT = '\n        <div class="dot"></div>\n        <div class="dot"></div>\n        <div class="dot"></div>\n    '
 
-    var msgSendingHandler = null;
+    var msgSendingHandler = null
 
     var vm = new Vue({
         el: '#mobile',
@@ -34,234 +34,234 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
             latestMsgContent: null
         },
 
-        mounted: function mounted() {
-            var _this = this;
+        mounted: function mounted () {
+            var _this = this
 
-            $.getJSON('./assets/dialog.json', function (data) {
-                _this.dialogs = data;
+            $.getJSON('/js/dialog.json', function (data) {
+                _this.dialogs = data
 
-                _this.nextTopics = _this.dialogs.fromUser;
+                _this.nextTopics = _this.dialogs.fromUser
 
-                _this.appendDialog('0000');
-            });
+                _this.appendDialog('0000')
+            })
         },
 
 
         methods: {
-            appendDialog: function appendDialog(id) {
-                var _this2 = this;
+            appendDialog: function appendDialog (id) {
+                var _this2 = this
 
                 if ((typeof id === 'undefined' ? 'undefined' : _typeof(id)) === 'object' && id.length > 0) {
                     // array of dialog ids
                     id.forEach(function (id) {
-                        return _this2.appendDialog(id);
-                    });
-                    return;
+                        return _this2.appendDialog(id)
+                    })
+                    return
                 } else if (id == null) {
                     // clear possible responses
-                    this.lastDialog.responses = null;
-                    return;
+                    this.lastDialog.responses = null
+                    return
                 }
 
-                this.isTyping = true;
+                this.isTyping = true
 
-                var dialog = this.getDialog(id);
+                var dialog = this.getDialog(id)
 
                 getRandomMsg(dialog.details).forEach(function (content) {
                     _this2.msgChain = _this2.msgChain.then(function () {
-                        return delay(700);
+                        return delay(700)
                     }).then(function () {
-                        return _this2.sendMsg(content, AUTHOR.AUTHOR);
-                    });
-                });
+                        return _this2.sendMsg(content, AUTHOR.AUTHOR)
+                    })
+                })
 
                 return dialog.nextAuthor ? this.appendDialog(dialog.nextAuthor) : this.msgChain.then(function () {
-                    _this2.lastDialog = dialog;
-                    _this2.isTyping = false;
-                });
+                    _this2.lastDialog = dialog
+                    _this2.isTyping = false
+                })
             },
-            sendMsg: function sendMsg(message, author) {
+            sendMsg: function sendMsg (message, author) {
                 switch (author) {
                     case 'me':
-                        return this.sendUserMsg(message);
+                        return this.sendUserMsg(message)
                     default:
-                        return this.sendFriendMsg(message, author);
+                        return this.sendFriendMsg(message, author)
                 }
             },
-            sendFriendMsg: function sendFriendMsg(message, author) {
-                var _this3 = this;
+            sendFriendMsg: function sendFriendMsg (message, author) {
+                var _this3 = this
 
-                var content = getRandomMsg(message);
-                var length = content.replace(/<[^>]+>/g, "").length;
-                var isImg = /<img[^>]+>/.test(content);
-                var isTyping = length > 2 || isImg;
+                var content = getRandomMsg(message)
+                var length = content.replace(/<[^>]+>/g, "").length
+                var isImg = /<img[^>]+>/.test(content)
+                var isTyping = length > 2 || isImg
 
                 var msg = {
                     author: author,
                     content: isTyping ? TYPING_MSG_CONTENT : content,
                     isImg: isImg
-                };
-                this.messages.push(msg);
+                }
+                this.messages.push(msg)
 
                 if (isTyping) {
-                    this.markMsgSize(msg);
-                    setTimeout(updateScroll);
+                    this.markMsgSize(msg)
+                    setTimeout(updateScroll)
 
                     return delay(Math.min(100 * length, 2000)).then(function () {
-                        return _this3.markMsgSize(msg, content);
+                        return _this3.markMsgSize(msg, content)
                     }).then(function () {
-                        return delay(150);
+                        return delay(150)
                     }).then(function () {
-                        msg.content = content;
-                        onMessageSending();
-                    });
+                        msg.content = content
+                        onMessageSending()
+                    })
                 }
 
-                onMessageSending();
+                onMessageSending()
 
-                return Promise.resolve();
+                return Promise.resolve()
             },
-            sendUserMsg: function sendUserMsg(message) {
+            sendUserMsg: function sendUserMsg (message) {
                 this.messages.push({
                     author: AUTHOR.ME,
                     content: message
-                });
+                })
 
-                onMessageSending();
+                onMessageSending()
 
-                return Promise.resolve();
+                return Promise.resolve()
             },
-            markMsgSize: function markMsgSize(msg) {
-                var _this4 = this;
+            markMsgSize: function markMsgSize (msg) {
+                var _this4 = this
 
-                var content = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+                var content = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null
 
-                this.latestMsgContent = content || msg.content;
+                this.latestMsgContent = content || msg.content
 
                 return delay(0).then(function () {
-                    return msg.isImg && onImageLoad($('#mock-msg img'));
+                    return msg.isImg && onImageLoad($('#mock-msg img'))
                 }).then(function () {
-                    Object.assign(msg, getMockMsgSize());
-                    _this4.messages = [].concat(_toConsumableArray(_this4.messages));
-                });
+                    Object.assign(msg, getMockMsgSize())
+                    _this4.messages = [].concat(_toConsumableArray(_this4.messages))
+                })
             },
-            getDialog: function getDialog(id) {
+            getDialog: function getDialog (id) {
                 // only one dialog should be matched by id
                 var dialogs = this.dialogs.fromMe.filter(function (dialog) {
-                    return dialog.id === id;
-                });
-                return dialogs ? dialogs[0] : null;
+                    return dialog.id === id
+                })
+                return dialogs ? dialogs[0] : null
             },
-            getDialogFromUser: function getDialogFromUser(id) {
+            getDialogFromUser: function getDialogFromUser (id) {
                 // only one dialog should be matched by id
                 var dialogs = this.dialogs.fromUser.filter(function (dialog) {
-                    return dialog.id === id;
-                });
-                return dialogs ? dialogs[0] : null;
+                    return dialog.id === id
+                })
+                return dialogs ? dialogs[0] : null
             },
-            togglePrompt: function togglePrompt(toShow) {
+            togglePrompt: function togglePrompt (toShow) {
                 if (this.isTyping) {
                     // don't prompt if author is typing
-                    return;
+                    return
                 }
 
-                this.hasPrompt = toShow;
+                this.hasPrompt = toShow
             },
-            respond: function respond(response) {
-                return this.say(response.content, response.nextAuthor);
+            respond: function respond (response) {
+                return this.say(response.content, response.nextAuthor)
             },
-            ask: function ask(fromUser) {
-                var content = getRandomMsg(fromUser.details);
-                return this.say(content, fromUser.nextAuthor);
+            ask: function ask (fromUser) {
+                var content = getRandomMsg(fromUser.details)
+                return this.say(content, fromUser.nextAuthor)
             },
-            say: function say(content, dialogId) {
-                var _this5 = this;
+            say: function say (content, dialogId) {
+                var _this5 = this
 
                 // close prompt
-                this.hasPrompt = false;
+                this.hasPrompt = false
 
                 return delay(200)
-                // send user msg
-                .then(function () {
-                    return _this5.sendMsg(content, AUTHOR.ME);
-                }).then(function () {
-                    return delay(300);
-                })
-                // add author's next dialogs
-                .then(function () {
-                    return _this5.appendDialog(dialogId);
-                });
+                    // send user msg
+                    .then(function () {
+                        return _this5.sendMsg(content, AUTHOR.ME)
+                    }).then(function () {
+                        return delay(300)
+                    })
+                    // add author's next dialogs
+                    .then(function () {
+                        return _this5.appendDialog(dialogId)
+                    })
             }
         }
-    });
+    })
 
     /**
      * get a random message from message array
      */
-    function getRandomMsg(messages) {
+    function getRandomMsg (messages) {
         // single item
         if (typeof messages === 'string' || !messages.length) {
-            return messages;
+            return messages
         }
 
-        var id = Math.floor(Math.random() * messages.length);
-        return messages[id];
+        var id = Math.floor(Math.random() * messages.length)
+        return messages[id]
     }
 
     /**
      * UI updating when new message is sending
      */
-    function onMessageSending() {
+    function onMessageSending () {
         setTimeout(function () {
             // update scroll position when vue has updated ui
-            updateScroll();
+            updateScroll()
 
-            var $latestMsg = $('#mobile-body-content .msg-row:last-child .msg');
+            var $latestMsg = $('#mobile-body-content .msg-row:last-child .msg')
 
             // add target="_blank" for links
-            $latestMsg.find('a').attr('target', '_blank');
+            $latestMsg.find('a').attr('target', '_blank')
 
             // update scroll position when images are loaded
-            onImageLoad($latestMsg).then(updateScroll);
-        });
+            onImageLoad($latestMsg).then(updateScroll)
+        })
     }
 
-    function updateScroll() {
-        var $chatbox = $('#mobile-body-content');
+    function updateScroll () {
+        var $chatbox = $('#mobile-body-content')
 
-        var distance = $chatbox[0].scrollHeight - $chatbox.height() - $chatbox.scrollTop();
-        var duration = 250;
-        var startTime = Date.now();
+        var distance = $chatbox[0].scrollHeight - $chatbox.height() - $chatbox.scrollTop()
+        var duration = 250
+        var startTime = Date.now()
 
-        requestAnimationFrame(function step() {
-            var p = Math.min(1, (Date.now() - startTime) / duration);
-            $chatbox.scrollTop($chatbox.scrollTop() + distance * p);
-            p < 1 && requestAnimationFrame(step);
-        });
+        requestAnimationFrame(function step () {
+            var p = Math.min(1, (Date.now() - startTime) / duration)
+            $chatbox.scrollTop($chatbox.scrollTop() + distance * p)
+            p < 1 && requestAnimationFrame(step)
+        })
     }
 
-    function delay() {
-        var amount = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+    function delay () {
+        var amount = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0
 
         return new Promise(function (resolve) {
-            setTimeout(resolve, amount);
-        });
+            setTimeout(resolve, amount)
+        })
     }
 
-    function getMockMsgSize() {
-        var $mockMsg = $('#mock-msg');
+    function getMockMsgSize () {
+        var $mockMsg = $('#mock-msg')
         return {
             width: $mockMsg.width(),
             height: $mockMsg.height()
-        };
+        }
     }
 
-    function onImageLoad($img) {
+    function onImageLoad ($img) {
         return new Promise(function (resolve) {
             $img.one('load', resolve).each(function (index, target) {
                 // trigger load when the image is cached
-                target.complete && $(target).trigger('load');
-            });
-        });
+                target.complete && $(target).trigger('load')
+            })
+        })
     }
-})();
+})()
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@
     const AUTHOR = {
         AUTHOR: 'author',
         ME: 'me'
-    };
+    }
 
     const TYPING_MSG_CONTENT = `
         <div class="dot"></div>
         <div class="dot"></div>
         <div class="dot"></div>
-    `;
+    `
 
-    let msgSendingHandler = null;
+    let msgSendingHandler = null
 
     const vm = new Vue({
         el: '#mobile',
@@ -32,227 +32,227 @@
             latestMsgContent: null
         },
 
-        mounted() {
-            $.getJSON('./assets/dialog.json', data => {
-                this.dialogs = data;
+        mounted () {
+            $.getJSON('/js/dialog.json', data => {
+                this.dialogs = data
 
-                this.nextTopics = this.dialogs.fromUser;
+                this.nextTopics = this.dialogs.fromUser
 
-                this.appendDialog('0000');
-            });
+                this.appendDialog('0000')
+            })
         },
 
         methods: {
-            appendDialog(id) {
+            appendDialog (id) {
                 if (typeof id === 'object' && id.length > 0) {
                     // array of dialog ids
-                    id.forEach(id => this.appendDialog(id));
-                    return;
+                    id.forEach(id => this.appendDialog(id))
+                    return
                 }
                 else if (id == null) {
                     // clear possible responses
-                    this.lastDialog.responses = null;
-                    return;
+                    this.lastDialog.responses = null
+                    return
                 }
 
-                this.isTyping = true;
+                this.isTyping = true
 
-                const dialog = this.getDialog(id);
+                const dialog = this.getDialog(id)
 
                 getRandomMsg(dialog.details)
                     .forEach(content => {
                         this.msgChain = this.msgChain
                             .then(() => delay(700))
-                            .then(() => this.sendMsg(content, AUTHOR.AUTHOR));
-                    });
+                            .then(() => this.sendMsg(content, AUTHOR.AUTHOR))
+                    })
 
                 return dialog.nextAuthor
                     ? this.appendDialog(dialog.nextAuthor)
                     : this.msgChain.then(() => {
-                        this.lastDialog = dialog;
-                        this.isTyping = false;
-                    });
+                        this.lastDialog = dialog
+                        this.isTyping = false
+                    })
             },
 
-            sendMsg(message, author) {
+            sendMsg (message, author) {
                 switch (author) {
                     case 'me':
-                        return this.sendUserMsg(message);
+                        return this.sendUserMsg(message)
                     default:
-                        return this.sendFriendMsg(message, author);
+                        return this.sendFriendMsg(message, author)
                 }
             },
 
-            sendFriendMsg(message, author) {
-                const content = getRandomMsg(message);
-                const length = content.replace(/<[^>]+>/g,"").length;
-                const isImg = /<img[^>]+>/.test(content);
-                const isTyping = length > 2 || isImg;
+            sendFriendMsg (message, author) {
+                const content = getRandomMsg(message)
+                const length = content.replace(/<[^>]+>/g, "").length
+                const isImg = /<img[^>]+>/.test(content)
+                const isTyping = length > 2 || isImg
 
                 const msg = {
                     author: author,
                     content: isTyping ? TYPING_MSG_CONTENT : content,
                     isImg: isImg
-                };
-                this.messages.push(msg);
+                }
+                this.messages.push(msg)
 
                 if (isTyping) {
-                    this.markMsgSize(msg);
-                    setTimeout(updateScroll);
+                    this.markMsgSize(msg)
+                    setTimeout(updateScroll)
 
                     return delay(Math.min(100 * length, 2000))
                         .then(() => {
-                            return this.markMsgSize(msg, content);
+                            return this.markMsgSize(msg, content)
                         })
                         .then(() => delay(150))
                         .then(() => {
-                            msg.content = content;
-                            onMessageSending();
-                        });
+                            msg.content = content
+                            onMessageSending()
+                        })
                 }
 
-                onMessageSending();
+                onMessageSending()
 
-                return Promise.resolve();
+                return Promise.resolve()
             },
 
-            sendUserMsg(message) {
+            sendUserMsg (message) {
                 this.messages.push({
                     author: AUTHOR.ME,
                     content: message
-                });
+                })
 
-                onMessageSending();
+                onMessageSending()
 
-                return Promise.resolve();
+                return Promise.resolve()
             },
 
-            markMsgSize(msg, content = null) {
-                this.latestMsgContent = content || msg.content;
+            markMsgSize (msg, content = null) {
+                this.latestMsgContent = content || msg.content
 
                 return delay(0)
                     .then(() => msg.isImg && onImageLoad($('#mock-msg img')))
                     .then(() => {
-                        Object.assign(msg, getMockMsgSize());
-                        this.messages = [...this.messages];
-                    });
+                        Object.assign(msg, getMockMsgSize())
+                        this.messages = [...this.messages]
+                    })
             },
 
-            getDialog(id) {
+            getDialog (id) {
                 // only one dialog should be matched by id
                 const dialogs = this.dialogs.fromMe
-                    .filter(dialog => dialog.id === id);
-                return dialogs ? dialogs[0] : null;
+                    .filter(dialog => dialog.id === id)
+                return dialogs ? dialogs[0] : null
             },
 
-            getDialogFromUser(id) {
+            getDialogFromUser (id) {
                 // only one dialog should be matched by id
                 const dialogs = this.dialogs.fromUser
-                    .filter(dialog => dialog.id === id);
-                return dialogs ? dialogs[0] : null;
+                    .filter(dialog => dialog.id === id)
+                return dialogs ? dialogs[0] : null
             },
 
-            togglePrompt(toShow) {
+            togglePrompt (toShow) {
                 if (this.isTyping) {
                     // don't prompt if author is typing
-                    return;
+                    return
                 }
 
-                this.hasPrompt = toShow;
+                this.hasPrompt = toShow
             },
 
-            respond(response) {
-                return this.say(response.content, response.nextAuthor);
+            respond (response) {
+                return this.say(response.content, response.nextAuthor)
             },
 
-            ask(fromUser) {
-                const content = getRandomMsg(fromUser.details);
-                return this.say(content, fromUser.nextAuthor);
+            ask (fromUser) {
+                const content = getRandomMsg(fromUser.details)
+                return this.say(content, fromUser.nextAuthor)
             },
 
-            say(content, dialogId) {
+            say (content, dialogId) {
                 // close prompt
-                this.hasPrompt = false;
+                this.hasPrompt = false
 
                 return delay(200)
                     // send user msg
                     .then(() => this.sendMsg(content, AUTHOR.ME))
                     .then(() => delay(300))
                     // add author's next dialogs
-                    .then(() => this.appendDialog(dialogId));
+                    .then(() => this.appendDialog(dialogId))
             }
         }
-    });
+    })
 
 
     /**
      * get a random message from message array
      */
-    function getRandomMsg(messages) {
+    function getRandomMsg (messages) {
         // single item
         if (typeof messages === 'string' || !messages.length) {
-            return messages;
+            return messages
         }
 
-        const id = Math.floor(Math.random() * messages.length);
-        return messages[id];
+        const id = Math.floor(Math.random() * messages.length)
+        return messages[id]
     }
 
 
     /**
      * UI updating when new message is sending
      */
-    function onMessageSending() {
+    function onMessageSending () {
         setTimeout(() => {
             // update scroll position when vue has updated ui
-            updateScroll();
+            updateScroll()
 
-            const $latestMsg = $('#mobile-body-content .msg-row:last-child .msg');
+            const $latestMsg = $('#mobile-body-content .msg-row:last-child .msg')
 
             // add target="_blank" for links
-            $latestMsg.find('a').attr('target', '_blank');
+            $latestMsg.find('a').attr('target', '_blank')
 
             // update scroll position when images are loaded
-            onImageLoad($latestMsg).then(updateScroll);
-        });
+            onImageLoad($latestMsg).then(updateScroll)
+        })
     }
 
-    function updateScroll() {
-        const $chatbox = $('#mobile-body-content');
+    function updateScroll () {
+        const $chatbox = $('#mobile-body-content')
 
-        const distance = $chatbox[0].scrollHeight - $chatbox.height() - $chatbox.scrollTop();
-        const duration = 250;
-        const startTime = Date.now();
+        const distance = $chatbox[0].scrollHeight - $chatbox.height() - $chatbox.scrollTop()
+        const duration = 250
+        const startTime = Date.now()
 
-        requestAnimationFrame(function step() {
-            const p = Math.min(1, (Date.now() - startTime) / duration);
-            $chatbox.scrollTop($chatbox.scrollTop() + distance * p);
-            p < 1 && requestAnimationFrame(step);
-        });
+        requestAnimationFrame(function step () {
+            const p = Math.min(1, (Date.now() - startTime) / duration)
+            $chatbox.scrollTop($chatbox.scrollTop() + distance * p)
+            p < 1 && requestAnimationFrame(step)
+        })
     }
 
-    function delay(amount = 0) {
+    function delay (amount = 0) {
         return new Promise(resolve => {
-            setTimeout(resolve, amount);
-        });
+            setTimeout(resolve, amount)
+        })
     }
 
-    function getMockMsgSize() {
-        const $mockMsg = $('#mock-msg');
+    function getMockMsgSize () {
+        const $mockMsg = $('#mock-msg')
         return {
             width: $mockMsg.width(),
             height: $mockMsg.height()
-        };
+        }
     }
 
-    function onImageLoad($img) {
+    function onImageLoad ($img) {
         return new Promise(resolve => {
             $img.one('load', resolve)
                 .each((index, target) => {
                     // trigger load when the image is cached
-                    target.complete && $(target).trigger('load');
-                });
-        });
+                    target.complete && $(target).trigger('load')
+                })
+        })
     }
 
-})();
+})()
